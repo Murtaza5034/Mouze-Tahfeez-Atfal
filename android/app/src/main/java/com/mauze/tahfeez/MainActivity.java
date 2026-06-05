e9ae8f887df77c4e3f59e2bbef25c12575946f19
Murtza Hamid <murtazahamid66@gmail.com>
2026-06-05 16:47:02 +0530

    fix: remove manual PushNotifications plugin registration from MainActivity - Capacitor auto-registers

--- android/app/src/main/java/com/mauze/tahfeez/MainActivity.java
+++ android/app/src/main/java/com/mauze/tahfeez/MainActivity.java
@@ -1,13 +1,5 @@
 package com.mauze.tahfeez;
 
-import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
-import com.getcapacitor.plugins.PushNotifications;
 
-public class MainActivity extends BridgeActivity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        registerPlugin(PushNotifications.class);
-        super.onCreate(savedInstanceState);
-    }
-}
+public class MainActivity extends BridgeActivity {}
